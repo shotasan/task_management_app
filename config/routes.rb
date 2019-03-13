715025e26cc9c9055988e7e56249347793cb4934
shotasan <wondermoai@gmail.com>
2019-03-13 16:27:00 +0900

    ログイン、ログアウト機能を実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "tasks#index"
   resources :tasks
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/login', to: 'sessions#destroy'
 end
