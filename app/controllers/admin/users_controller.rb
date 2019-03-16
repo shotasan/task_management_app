0049b9e01747439bbe3b51351d8b9831f0cd4095
shotasan <wondermoai@gmail.com>
2019-03-16 17:39:13 +0900

    管理者用のコントローラを作成。管理者登録画面を作成。

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,31 @@
+class Admin::UsersController < ApplicationController
+  skip_before_action :login_required
+  
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to admin_user_path, notice: "ユーザー「#{@user.name}を登録しました"
+    else
+      render :new
+    end
+  end
+  def edit
+  end
+
+  def show
+  end
+
+  def index
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :admin, :password, :password_confirmation)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root "tasks#index"
+  namespace :admin do
+    resources :users
+  end
   resources :tasks
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
