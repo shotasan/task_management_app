848974b928573cbcca18ad3e84026f555d86b3f5
shotasan <wondermoai@gmail.com>
2019-03-18 12:28:13 +0900

    管理者画面でユーザー一覧を作成

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,31 +1,49 @@
 class Admin::UsersController < ApplicationController
   skip_before_action :login_required
+  before_action :set_user, only: [:show, :edit, :destroy]
+
+  def index
+    @users = User.all
+  end
   
+  def show
+  end
+
   def new
     @user = User.new
+  end
+
+  def edit
   end
 
   def create
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to admin_user_path, notice: "ユーザー「#{@user.name}を登録しました"
+      redirect_to admin_users_path, notice: "ユーザー「#{@user.name}」を登録しました"
     else
+      binding.pry
       render :new
     end
   end
-  def edit
+
+  def update
+    if @user.update(user_params)
+      redirect_to admin_user_path(@user), notice: "ユーザー「#{@user.name}」を更新しました"
+    end
   end
 
-  def show
+  def destroy
+    @user.destroy
+    redirect_to admin_users_path, notice: "ユーザー「#{@user.name}」を削除しました"
   end
-
-  def index
-  end
-
   private
 
   def user_params
     params.require(:user).permit(:name, :email, :admin, :password, :password_confirmation)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
