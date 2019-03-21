1f73c234f32edb750af2d2acdccaa1b6fd7977ce
shotasan <wondermoai@gmail.com>
2019-03-21 11:43:31 +0900

    管理者画面のユーザー一覧から、一般ユーザーを削除できない問題を解消

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   before_validation { email&.downcase! }
-  before_destroy :deleting_ok?
+  before_destroy :admin_user?
 
   validates :name, presence: true, length: { maximum: 30 }
   validates :email, presence: true, uniqueness: true, length: { maximum: 255 },
@@ -13,6 +13,11 @@ class User < ApplicationRecord
 
   private
 
+  # 削除対象ユーザーが管理者権限を持っているかの判定
+  def admin_user?
+    deleting_ok? if self.admin == true
+  end
+  
   # adminがtrueのユーザー（管理者権限あり）がゼロにならないようにエラーを発生させる
   def deleting_ok?
     users = User.all
