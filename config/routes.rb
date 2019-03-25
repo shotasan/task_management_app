e44158ffde0b85ecf87f7d96c975a4ea3f44b9c8
shotasan <wondermoai@gmail.com>
2019-03-25 12:02:54 +0900

    gemファイルの修正、ルートの修正。

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root "tasks#index"
   namespace :admin do
     resources :users
-    resources :labels
+    resources :labels, only: [:index, :create, :destroy]
   end
   resources :tasks
   resources :users
