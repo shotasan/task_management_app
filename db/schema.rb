711efe29ce628326c30860f9361c87272d49356e
shotasan <wondermoai@gmail.com>
2019-03-31 16:36:53 +0900

    締め切りのデフォルト値を変更。タスクの新規作成画面で締切日に今日の日付が入るようにビューを修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_20_114340) do
+ActiveRecord::Schema.define(version: 2019_03_30_053117) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,7 +33,7 @@ ActiveRecord::Schema.define(version: 2019_03_20_114340) do
     t.string "content", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.date "limit_date", default: "2019-03-06", null: false
+    t.date "limit_date", null: false
     t.string "status", default: "未着手", null: false
     t.integer "priority", default: 0, null: false
     t.bigint "user_id"
