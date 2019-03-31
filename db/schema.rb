95dfcc8f27739576ebbffbbf1c16af9050e868b2
shotasan <wondermoai@gmail.com>
2019-03-31 21:17:55 +0900

    Groupモデルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_30_053117) do
+ActiveRecord::Schema.define(version: 2019_03_31_121610) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "groups", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "labels", force: :cascade do |t|
     t.string "title", null: false
